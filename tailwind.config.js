e05f5e85d67311f9238ada204080363902e6ff34
Valz <revalfahrizal87@gmail.com>
2025-02-26 15:35:43 +0700

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      poppins: "Poppins",
+    },
+    extend: {
+
+      colors: {
+        primary: "#FF725E",
+        secondary: "#DFB4BD",
+        putih: "#FFFFFD",
+      },
+    },
+  },
+  plugins: [],
+};
